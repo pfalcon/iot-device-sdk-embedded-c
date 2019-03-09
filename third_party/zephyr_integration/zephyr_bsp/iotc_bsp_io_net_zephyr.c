5a71a6e1fc1041624c08a05262b7c8e8967f2381
Kumar Gala <kumar.gala@linaro.org>
2019-03-09 13:09:48 -0600

    move to using zsock names
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- third_party/zephyr_integration/zephyr_bsp/iotc_bsp_io_net_zephyr.c
+++ third_party/zephyr_integration/zephyr_bsp/iotc_bsp_io_net_zephyr.c
@@ -35,8 +35,8 @@ iotc_bsp_io_net_state_t
 iotc_bsp_io_net_socket_connect(iotc_bsp_socket_t* iotc_socket, const char* host,
                                uint16_t port,
                                iotc_bsp_socket_type_t socket_type) {
-  struct addrinfo hints;
-  struct addrinfo *result, *rp = NULL;
+  struct zsock_addrinfo hints;
+  struct zsock_addrinfo *result, *rp = NULL;
   int status;
 
   memset(&hints, 0, sizeof(hints));
@@ -46,20 +46,20 @@ iotc_bsp_io_net_socket_connect(iotc_bsp_socket_t* iotc_socket, const char* host,
   hints.ai_protocol = 0;
 
   // Address resolution.
-  status = getaddrinfo(host, NULL, &hints, &result);
+  status = zsock_getaddrinfo(host, NULL, &hints, &result);
   if (0 != status) {
     return IOTC_BSP_IO_NET_STATE_ERROR;
   }
 
   for (rp = result; rp != NULL; rp = rp->ai_next) {
-    *iotc_socket = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+    *iotc_socket = zsock_socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
     if (-1 == *iotc_socket)
       continue;
 
     // Set the socket to be non-blocking
-    const int flags = fcntl(*iotc_socket, F_GETFL, 0);
-    if (-1 == fcntl(*iotc_socket, F_SETFL, flags | O_NONBLOCK)) {
-      freeaddrinfo(result);
+    const int flags = zsock_fcntl(*iotc_socket, F_GETFL, 0);
+    if (-1 == zsock_fcntl(*iotc_socket, F_SETFL, flags | O_NONBLOCK)) {
+      zsock_freeaddrinfo(result);
       return IOTC_BSP_IO_NET_STATE_ERROR;
     }
 
@@ -76,21 +76,21 @@ iotc_bsp_io_net_socket_connect(iotc_bsp_socket_t* iotc_socket, const char* host,
     }
 
     // Attempt to connect.
-    status = connect(*iotc_socket, rp->ai_addr, rp->ai_addrlen);
+    status = zsock_connect(*iotc_socket, rp->ai_addr, rp->ai_addrlen);
 
     if (-1 != status) {
-      freeaddrinfo(result);
+      zsock_freeaddrinfo(result);
       return IOTC_BSP_IO_NET_STATE_OK;
     } else {
       if (EINPROGRESS == errno) {
-        freeaddrinfo(result);
+        zsock_freeaddrinfo(result);
         return IOTC_BSP_IO_NET_STATE_OK;
       } else {
-        close(*iotc_socket);
+        zsock_close(*iotc_socket);
       }
     }
   }
-  freeaddrinfo(result);
+  zsock_freeaddrinfo(result);
   return IOTC_BSP_IO_NET_STATE_ERROR;
 }
 
@@ -112,7 +112,7 @@ iotc_bsp_io_net_state_t iotc_bsp_io_net_write(iotc_bsp_socket_t iotc_socket,
   }
 
   int errval = 0;
-  *out_written_count = send(iotc_socket, buf, count, 0);
+  *out_written_count = zsock_send(iotc_socket, buf, count, 0);
 
   if (*out_written_count < 0) {
     *out_written_count = 0;
@@ -142,7 +142,7 @@ iotc_bsp_io_net_state_t iotc_bsp_io_net_read(iotc_bsp_socket_t iotc_socket,
   }
 
   int errval = 0;
-  *out_read_count = recv(iotc_socket, buf, count, MSG_DONTWAIT);
+  *out_read_count = zsock_recv(iotc_socket, buf, count, ZSOCK_MSG_DONTWAIT);
 
   if (*out_read_count < 0) {
     *out_read_count = 0;
@@ -174,7 +174,7 @@ iotc_bsp_io_net_close_socket(iotc_bsp_socket_t* iotc_socket) {
     return IOTC_BSP_IO_NET_STATE_ERROR;
   }
 
-  close(*iotc_socket);
+  zsock_close(*iotc_socket);
 
   *iotc_socket = 0;
 
@@ -218,7 +218,7 @@ iotc_bsp_io_net_select(iotc_bsp_socket_events_t* socket_events_array,
   }
 
   /* call the actual posix select */
-  const int result = poll(fds, 1, timeout_sec);
+  const int result = zsock_poll(fds, 1, timeout_sec);
 
   if (0 < result) {
     /* translate the result back to the socket events structure */
