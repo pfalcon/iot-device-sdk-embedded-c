37530f0f48799f3b87328c0e2fd9292669cbe94e
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2019-04-23 20:27:50 +0300

    iotc_bsp_time_zephyr: Acquire absolute time via SNTP on startup
    Generating JWT token to authenticate to Google Cloud requires absolute
    wall clock time. Acquire it using Zephyr SNTP API, and calculate
    correct current time using it as the timebase afterwards.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- third_party/zephyr_integration/zephyr_bsp/iotc_bsp_time_zephyr.c
+++ third_party/zephyr_integration/zephyr_bsp/iotc_bsp_time_zephyr.c
@@ -14,13 +14,35 @@
  */
 
 #include <iotc_bsp_time.h>
+#include <stdio.h>
 #include <zephyr.h>
+#include <net/sntp.h>
+#include <time.h>
 
-void iotc_bsp_time_init() { /* empty */
+u64_t time_base;
+
+void iotc_bsp_time_init() {
+  struct sntp_time ts;
+  int res = sntp_simple("time.nist.gov", 3000, &ts);
+
+  if (res < 0) {
+    printf("Cannot acquire current time\n");
+    exit(1);
+  }
+
+  time_base = ts.seconds - k_uptime_get() / MSEC_PER_SEC;
+
+  printf("Acquired current time: %u\n", (unsigned)time_base);
+
+  struct timespec tspec;
+  tspec.tv_sec = ts.seconds;
+  tspec.tv_nsec = ((u64_t)ts.fraction * (1000 * 1000 * 1000)) >> 32;
+  res = clock_settime(CLOCK_REALTIME, &tspec);
+  printf("clock_settime: %d, errno: %d\n", res, errno);
 }
 
 iotc_time_t iotc_bsp_time_getcurrenttime_seconds() {
-  return (k_uptime_get() + (MSEC_PER_SEC/2)) / MSEC_PER_SEC;
+  return (k_uptime_get() + (MSEC_PER_SEC/2)) / MSEC_PER_SEC + time_base;
 }
 
 iotc_time_t iotc_bsp_time_getcurrenttime_milliseconds() {
